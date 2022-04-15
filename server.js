d83e67439e97114738c02978e8442973312eeb2c
MuhammadIkram24 <m.ikramch22@gmail.com>
2022-04-16 04:29:47 +0500

    updated db.json file

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3500;
 
 server.use(middlewares);
 server.use(router);
